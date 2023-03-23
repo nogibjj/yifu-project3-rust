c2ab029313e548e609f2140eed475e25ff3ec817
yifu <77519205+yifu-deng@users.noreply.github.com>
2023-03-23 04:38:22 +0000

    main

--- RSP/src/main.rs
+++ RSP/src/main.rs
@@ -1,121 +1,41 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body("Hello AWS Lambda HTTP request".into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use lambda_http::{lambda, IntoResponse, Request, RequestExt};
 use rand::Rng;
+use serde::Deserialize;
 use std::cmp::Ordering;
 use std::cmp::{Ord, PartialOrd};
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
-enum Choice {
-    Rock,
-    Paper,
-    Scissors,
+#[derive(Debug, Deserialize)]
+struct QueryParams {
+    choice: String,
 }
 
-impl Eq for Choice {}
+async fn play_game(event: Request) -> Result<impl IntoResponse, lambda_http::Error> {
+    // Parse query parameters
+    let query_params: QueryParams = match event.query_string_parameters() {
+        Some(params) => serde_urlencoded::from_str(params)?,
+        None => return Ok("Invalid choice. Choose rock, paper, or scissors.".into_response()),
+    };
 
-impl PartialOrd for Choice {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+    let player_choice = match Choice::from_str(&query_params.choice) {
+        Ok(choice) => choice,
+        Err(_) => return Ok("Invalid choice. Choose rock, paper, or scissors.".into_response()),
+    };
+
+    let computer_choice = rand::thread_rng().gen_range(0..3);
+    let computer_choice = match computer_choice {
+        0 => Choice::Rock,
+        1 => Choice::Paper,
+        _ => Choice::Scissors,
+    };
+
+    let result = determine_winner(player_choice, computer_choice);
+    Ok(result.into_response())
 }
-
-impl Ord for Choice {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match (self, other) {
-            (Choice::Rock, Choice::Scissors) => Ordering::Greater,
-            (Choice::Scissors, Choice::Paper) => Ordering::Greater,
-            (Choice::Paper, Choice::Rock) => Ordering::Greater,
-            (Choice::Scissors, Choice::Rock) => Ordering::Less,
-            (Choice::Paper, Choice::Scissors) => Ordering::Less,
-            (Choice::Rock, Choice::Paper) => Ordering::Less,
-            _ => Ordering::Equal,
-        }
-    }
-}
-
-fn determine_winner(player: Choice, computer: Choice) -> String {
-    match player.cmp(&computer) {
-        Ordering::Equal => "It's a tie!".to_string(),
-        Ordering::Greater => "You win!".to_string(),
-        Ordering::Less => "Computer wins!".to_string(),
-    }
-}
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Welcome to Rock, Paper, Scissors!\nPlease input your choice after the slash in the URL.(e.g. /rock, /paper, /scissors)")
-}
-impl FromStr for Choice {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "rock" => Ok(Choice::Rock),
-            "paper" => Ok(Choice::Paper),
-            "scissors" => Ok(Choice::Scissors),
-            _ => Err(()),
-        }
-    }
-}
-
-async fn play_game(choice: web::Path<String>) -> impl Responder {
-    match Choice::from_str(&choice) {
-        Ok(player_choice) => {
-            let computer_choice = rand::thread_rng().gen_range(0..3);
-            let computer_choice = match computer_choice {
-                0 => Choice::Rock,
-                1 => Choice::Paper,
-                _ => Choice::Scissors,
-            };
-
-            let result = determine_winner(player_choice, computer_choice);
-            HttpResponse::Ok().body(result)
-        }
-        Err(_) => {
-            HttpResponse::BadRequest().body("Invalid choice. Choose rock, paper, or scissors.")
-        }
-    }
-}
-
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .service(hello)
-//             .service(web::resource("/{choice}").to(play_game))
-//     })
-//     .bind("127.0.0.1:8080")?
-//     .run()
-//     .await
-// }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
-        .without_time()
-        .init();
-
-    run(service_fn(function_handler)).await
+async fn main() -> Result<(), lambda_http::Error> {
+    lambda::run(handler(function_handler)).await?;
+    Ok(())
 }
